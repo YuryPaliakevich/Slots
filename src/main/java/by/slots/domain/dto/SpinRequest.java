ba2fcca226f615e72ada5f16ee0f92dca12809a4
YuryPaliakevich <yury.paliakevich@gmail.com>
2023-10-16 21:46:53 +0300

    added lombok + jakarta validation

--- src/main/java/by/slots/domain/dto/SpinRequest.java
+++ src/main/java/by/slots/domain/dto/SpinRequest.java
@@ -1,8 +1,14 @@
 package by.slots.domain.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
 
+@Data
 public class SpinRequest {
 
+    @Min(value = 25)
+    @Max(value = 1000)
     private int stake;
 
 }
--- src/main/java/by/slots/rest/SlotsController.java
+++ src/main/java/by/slots/rest/SlotsController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import by.slots.domain.dto.SpinRequest;
+import jakarta.validation.Valid;
+
 @RestController("/slots")
 public class SlotsController {
 
     @RequestMapping(value = "/spin", method = RequestMethod.POST)
-    public void spin() {
+    public void spin(@Valid SpinRequest spinRequest) {
 
     }
 
